d71b2f33ddd4269ca1d43b2da70578680a820976
Luis Padilla <lpd1211@gmail.com>
2021-02-14 20:17:49 -0600

    updates

--- faker/index.ts
+++ faker/index.ts
@@ -1,23 +1,35 @@
 import * as fs from 'fs'
 
-import * as faker from 'faker'
+import faker from 'faker'
 faker.locale = 'es'
 import {resolve, extend} from 'json-schema-faker'
+import ObjToCsv  from 'objects-to-csv'
 
 import composedSchema, {models} from './models'
 
 console.log('Schema', composedSchema)
 
+const type = 'csv'
+const entity = 'colaboradores'
+
 extend('faker', () => faker)
-resolve(composedSchema, models).then((result) => {
+
+resolve(composedSchema.properties.colaboradores, models).then(async (result) => {
   console.log('RESULT', result)
-  fs.writeFile(`${__dirname}/records.json`, JSON.stringify(result), (err) => {
-    if (err) {
-      // eslint-disable-next-line no-console
-      console.error(err)
-    } else {
-      // eslint-disable-next-line no-console
-      console.log('wrote to records.json')
-    }
-  })
+  const csv = new ObjToCsv(result)
+  console.log('csv', csv)
+  const filePath = `${__dirname}/${entity}`
+  if(type === 'csv'){
+    await csv.toDisk(`${filePath}.csv`)
+  } else {
+    fs.writeFile(`${filePath}.json`, JSON.stringify(result), (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.error(err)
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('wrote to records.json')
+      }
+    })
+  }
 })
